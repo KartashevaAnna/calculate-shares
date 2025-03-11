1d82ab2ecbbd3436d8c94f5dc7c9480966778332
Anna Veselova <annakartashevamail@gmail.com>
2025-03-11 13:08:59 +0300

    improved structure

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-import xlsxwriter
+from write_to_excel import write_to_excel
+from write_to_txt_file import write_to_txt_file
 from typing import Dict, List
-from all_shares import ALL_SHARES
+from to_ignore_by_git.all_shares import ALL_SHARES
 from pydantic import (
     BaseModel,
     computed_field,
@@ -47,39 +48,7 @@ def know_shares(shares_list: List[Dict[str, int]]):
     return result
 
 
-def write_to_file(resulting_shares: List[Dict[str, int]]):
-    with open("RESULT.txt", "w+", encoding="utf-8") as f:
-        for record in resulting_shares:
-            f.write(str(record) + "\n")
-
-
-def write_to_excel(resulting_shares: List[Dict[str, int]]):
-    workbook = xlsxwriter.Workbook("RESULT.xlsx")
-    worksheet = workbook.add_worksheet()
-
-    row = 0
-    column = 0
-    col_names = [
-        "Название",
-        "Всего акций",
-        "Акций каждому",
-        "Акций Ане",
-        "Разделить после получения",
-    ]
-    for name in col_names:
-        worksheet.write(row, column, name)
-        column += 1
-    for item in resulting_shares:
-        column = 0
-        row += 1
-        for key in item:
-            worksheet.write(row, column, item[key])
-            column += 1
-    worksheet.autofit()
-    workbook.close()
-
-
 if __name__ == "__main__":
     resulting_shares = know_shares(ALL_SHARES)
-    write_to_file(resulting_shares)
+    write_to_txt_file(resulting_shares)
     write_to_excel(resulting_shares)
---
+++ write_to_excel.py
@@ -0,0 +1,28 @@
+import xlsxwriter
+from typing import Dict, List
+
+
+def write_to_excel(resulting_shares: List[Dict[str, int]]):
+    workbook = xlsxwriter.Workbook("to_ignore_by_git/RESULT.xlsx")
+    worksheet = workbook.add_worksheet()
+
+    row = 0
+    column = 0
+    col_names = [
+        "Название",
+        "Всего акций",
+        "Акций каждому",
+        "Акций Ане",
+        "Разделить после получения",
+    ]
+    for name in col_names:
+        worksheet.write(row, column, name)
+        column += 1
+    for item in resulting_shares:
+        column = 0
+        row += 1
+        for key in item:
+            worksheet.write(row, column, item[key])
+            column += 1
+    worksheet.autofit()
+    workbook.close()
